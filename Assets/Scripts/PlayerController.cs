69f7ac2a802191aabeb03242a25c9654b33ac92d
Heizal <patriciaheizal@gmail.com>
2023-07-16 14:07:42 +0200

    New changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Initialize speed variable
-    public float speed = 5.0f;
-
+    private float speed = 15.0f;
+    //Initialize left and right speed variable
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +20,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //move vehicle with left and right keys
+        horizontalInput = Input.GetAxis("Horizontal");
+        //move vehicle with forward and backwards keys
+        forwardInput = Input.GetAxis("Vertical");
+
         //move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //move vehicle left and right
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
         
     }
 }
