e282c048a9ba3411326b8dc4a307a8ee813e77a1
Heizal <patriciaheizal@gmail.com>
2023-07-16 14:12:15 +0200

    add files

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //Offset camera position behind car by adding to the players position
+        transform.position = player.transform.position + offset;
+        
+    }
+}
